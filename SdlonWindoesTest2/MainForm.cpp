30b7ce0703e036ba177c7852e601e05147773ec9
Adolfo Schalk <schalk_asc@hotmail.com>
2024-03-19 15:30:20 +0000

    Prototype
    Prototype

---
+++ SdlonWindoesTest2/MainForm.cpp
@@ -0,0 +1,26 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+
+
+void Main()
+{
+
+
+  
+
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	SdlonWindoesTest2::MainForm frm;
+	Application::Run(% frm);
+
+}
+
+
+
+
+
+
